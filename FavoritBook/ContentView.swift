77663501d454ce314019e20a26237c7f7a997f7f
nureddinelmas <nureddinelmas34@gmail.com>
2021-12-12 18:56:08 +0100

    Initial Commit

---
+++ FavoritBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoritBook
+//
+//  Created by Nureddin Elmas on 2021-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavoritBook/FavoritBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritBookApp.swift
+//  FavoritBook
+//
+//  Created by Nureddin Elmas on 2021-12-12.
+//
+
+import SwiftUI
+
+@main
+struct FavoritBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
